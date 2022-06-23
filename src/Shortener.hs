286930fe77c39a5b2c5f4b15571d630312a92f17
Christian <csamaniego@stackbuilders.com>
2022-06-23 16:12:04 -0500

    initial commit

---
+++ src/Shortener.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Shortener where
+
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import qualified Text.Blaze.Html5 as H
+import Web.Scotty
+
+shortener :: IO ()
+shortener =
+  scotty 3000 $
+    get "/" $
+      html $ renderHtml $
+        H.html $
+          H.body $ do
+            H.h1 "Shortener"
