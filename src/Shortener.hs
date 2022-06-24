d49ca355f0488a8fe589ea67bd5a8a7942e01129
Christian <csamaniego@stackbuilders.com>
2022-06-24 10:55:27 -0500

    Added post route

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -2,15 +2,31 @@
 
 module Shortener where
 
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.IORef (modifyIORef, newIORef)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text (Text)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
 
 shortener :: IO ()
-shortener =
-  scotty 3000 $
+shortener = do
+  urlsR <- newIORef (1 :: Int, mempty :: Map Int Text)
+  scotty 3000 $ do
     get "/" $
       html $ renderHtml $
         H.html $
           H.body $ do
             H.h1 "Shortener"
+            H.form H.! A.method "post" H.! A.action "/" $ do
+              H.input H.! A.type_ "text" H.! A.name "url"
+              H.input H.! A.type_ "submit"
+    post "/" $ do
+      url <- param "url"
+      liftIO $ modifyIORef urlsR $
+        \(i, urls) ->
+          (i + 1, M.insert i url urls)
+      redirect "/"
